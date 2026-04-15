9cab4116f6ebb3b2a16eae50e85ee403a867c4b1
g.zambrana <gabrielz022604@gmail.com>
2026-04-15 10:16:44 -0400

    made function to add random amount of squares on screen and random color and positiion

---
+++ squares.js
@@ -0,0 +1,22 @@
+let squareArea = document.querySelector("#squarearea");
+
+let squareCount = parseInt(Math.random()*21)+30;
+
+for (let i=0; i < squareCount; i++){
+    let square = document.createElement("div");
+    square.className = "square";
+    square.style.top = parseInt(Math.random()*250)+"px";
+    square.style.left = parseInt(Math.random()*650)+"px";
+    square.style.backgroundColor = getRandomColor();
+    squareArea.append(square);
+}
+
+function getRandomColor(){
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i =0; i < 6; i++){
+        color += letters.charAt(parseInt(Math.random()*letters.length));
+    }
+
+    return color;
+}
