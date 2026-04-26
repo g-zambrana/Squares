4609718d36e787ba05c64a988aee7f84a5dafa68
g.zambrana <gabrielz022604@gmail.com>
2026-04-25 20:23:41 -0400

    added onload function using previous addSquare() function

--- squares.js
+++ squares.js
@@ -2,13 +2,10 @@ let squareArea = document.querySelector("#squarearea");
 
 let squareCount = parseInt(Math.random()*21)+30;
 
-for (let i=0; i < squareCount; i++){
-    let square = document.createElement("div");
-    square.className = "square";
-    square.style.top = parseInt(Math.random()*250)+"px";
-    square.style.left = parseInt(Math.random()*650)+"px";
-    square.style.backgroundColor = getRandomColor();
-    squareArea.append(square);
+window.onload = function(){
+    for(let i=0; i < squareCount; i++){
+        addSquare()
+    }
 }
 
 function getRandomColor(){
